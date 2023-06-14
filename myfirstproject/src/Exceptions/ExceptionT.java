706decd944213e36d3ea542b176edb1f73fe8957
srinath51 <136569126+srinath51@users.noreply.github.com>
2023-06-14 15:23:46 +0530

    Update ExceptionT.java

--- myfirstproject/src/Exceptions/ExceptionT.java
+++ myfirstproject/src/Exceptions/ExceptionT.java
@@ -2,10 +2,10 @@ package Exceptions;
 
 public class ExceptionT {
 	public static void main(String[] args) {
-		int ad=90;
+		int d=909;
 		int b=0;
 		try {
-		int c=ad/b;
+		int c=d/b;
 		System.out.println(c);
 		}
 		catch(Exception s) {
