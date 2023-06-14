99a9ed77748035e5b4e267804898c5acd449c02e
srinathreddy76 <136448115+srinathreddy76@users.noreply.github.com>
2023-06-14 12:56:36 +0530

    Update collections.java

--- myfirstproject/src/collection/collections.java
+++ myfirstproject/src/collection/collections.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class collections {
 public static void main(String [] args) {
 	Set<Integer> s=new HashSet<Integer>();
-	s.add(90);
+	s.add(900);
 	s.add(89);
 	System.out.println("set"+s);
 	List<Integer> a=new ArrayList<Integer>(s);
