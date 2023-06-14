98bef1e1b955b88eee76355d4999fe4ee260ba9b
srinathreddy76 <136448115+srinathreddy76@users.noreply.github.com>
2023-06-14 15:13:20 +0530

    Update display.java

--- myfirstproject/src/methodoverloading/display.java
+++ myfirstproject/src/methodoverloading/display.java
@@ -1,21 +1,21 @@
 package methodoverloading;
 
 public class display {
-	public void show(int a,double b) {
-		System.out.println(a);
+	public void show(int c,double b) {
+		System.out.println(c);
 		System.out.println(b);
 	}
-	public void show(String a,int b) {
-		System.out.println(a);
+	public void show(String c,int b) {
+		System.out.println(c);
 		System.out.println(b);
 	}
-	public void show(boolean a,double b) {
-		System.out.println(a);
+	public void show(boolean c,double b) {
+		System.out.println(c);
 		System.out.println(b);
 	}
 	public static void main(String[] args) {
 		display d1=new display();
-		d1.show("hello", 90);
+		d1.show("hii", 900);
 		d1.show(10,8.9);
 		d1.show(true, 78.6);
 	}
